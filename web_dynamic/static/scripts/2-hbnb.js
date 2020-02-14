7d8f0da03dbf8ffd7364662cbe03ac159cf51da1
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-14 16:14:20 -0500

    Feature: Added behavior from 1-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,4 +14,19 @@ $(function () {
       apiStatusBox.removeClass('available');
     }
   });
+
+  const amens = {};
+
+  $('input[type=checkbox]').change(function () {
+    if ($(this).is(':checked')) {
+      amens[$(this).data().id] = $(this).data().name;
+    } else {
+      delete amens[$(this).data().id];
+    }
+    const names = [];
+    for (const name in amens) {
+      names.push(amens[name]);
+    }
+    $('DIV.amenities H4').text(names.join(', '));
+  });
 });
